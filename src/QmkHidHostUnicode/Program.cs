97e62e9f1445f220c23c6c6d1e511c42bd6e5c7e
Stefano <schmidtsigma@gmail.com>
2024-10-07 20:23:06 +0300

    add start-up code

--- src/QmkHidHostUnicode/Program.cs
+++ src/QmkHidHostUnicode/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace QmkHidHostUnicode;
+﻿using Microsoft.Extensions.Configuration;
+using QmkHidHostUnicode.UnicodeSenders;
+using System.Collections.Generic;
+using QmkHidHostUnicode.Options;
+using System.Threading;
+using System.IO;
+using Serilog;
+using System;
+
+namespace QmkHidHostUnicode;
 
 public class Program
 {
-	public static void Main()
+	private static void Main()
 	{
+		try { HelloWorld(); }
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message + "\r\nPress any key to close the console");
+			Console.ReadKey();
+		}
+	}
+	
+	private static void HelloWorld()
+	{
+		var config = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
 		
+		var sender = UnicodeSelector.GetSender();
+		var logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+		
+		var threads = new List<Thread>();
+		
+		foreach (var childSection in config.GetSection(DeviceOptions.Section).GetChildren())
+		{
+			var deviceOpts = new DeviceOptions();
+			childSection.Bind(deviceOpts);
+			
+			var unicodeListener = new UnicodeListener(deviceOpts, sender, logger);
+			
+			var thread = new Thread(unicodeListener.StartListening);
+			thread.Start();
+			threads.Add(thread);
+		}
+		
+		foreach (var thread in threads)
+			thread.Join();
 	}
 }
